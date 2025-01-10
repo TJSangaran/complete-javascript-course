dacb32cf0c6f65c82c7229d55d694aa01306595e
tjsangaran <theavarasasangaran@gmail.com>
2025-01-10 20:12:35 +0530

    Practice

---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,53 @@
+// let firstName = "Sangaran"
+
+// console.log(firstName)
+// console.log(firstName)
+// console.log(firstName)
+
+// let jonas_matilda = "JM"
+// let $function = 27;
+
+// let persin = 'jonas'
+
+// let conuntry = "Sri Lanka"
+// let continent = "Asian"
+// let population = "20 Million"
+
+// let x = y = 10;
+// console.log(x, y)
+// y = 20
+// console.log(x, y)
+
+// // ! Coding challenges
+
+// // * Challenge 1
+
+// const massMark = 76
+// const heightMark = 1.65
+
+// const massJhon = 85
+// const heightJhon = 1.83
+
+// const BMIMark = massMark / (heightMark ** 2)
+// const BMIJhon = massJhon / (heightJhon ** 2)
+
+// const markHigherBMI = markBMI > jhonBMI
+
+// console.log(markHigherBMI)
+
+// console.log(10 + 8 + '2')
+// const n = 12;
+
+// switch (n) {
+//     case 10:
+//         console.log("10");
+//         break;
+//     case 11:
+//     case 12:
+//         console.log("11 or 12");
+//         break;
+//     default:
+//         console.log("Default")
+// }
+const age = 19
+console.log(`I' am drinking ${age >= 18 ? 'wine' : 'water'}`)
