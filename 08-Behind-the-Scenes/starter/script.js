2c59262e089225f7c048d30a75be60486cd09b89
TJSangaran <thevarasasangaran@gmail.com>
2025-01-23 23:43:19 +0530

    Practice until the java script working method

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+// function cacAge(birthYear) {
+//   const age = 2021 - birthYear;
+
+//   function printAge() {
+//     const output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+//     return age;
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// cacAge(1991);
+
+// console.log(calcAge)
+
+// function calcAge(){
+//   return 2021 - birthYear;
+// }
+
+// console.log(x)
+// const x = 23;
+// console.log(x)
+
+
+console.log(me)
+console.log(job)
+console.log(year)
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
